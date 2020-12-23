28ea985e2c66c7e7f742b1742c66809fd3b403bb
Ravindra R. Jaju <jaju@msync.org>
2020-12-23 21:18:59 +0530

    Converted spring libs dependencies to 'provided' scope
    Updated description and URL.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (def spring-version "5.3.2")
 
 (defproject org.msync/spring-boot-bugger "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Just add dependency, sprinkle some config, and run Clojure in your Springboot application."
+  :url "https://github.com/jaju/spring-boot-bugger"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :profiles {:provided
@@ -11,8 +11,8 @@
                [org.springframework/spring-web ~spring-version]]}}
   :dependencies [[org.clojure/clojure "1.10.2-rc1"]
                  [nrepl/nrepl "0.8.3"]
-                 [org.springframework/spring-context ~spring-version]
-                 [org.springframework/spring-web ~spring-version]]
+                 [org.springframework/spring-context ~spring-version :scope "provided"]
+                 [org.springframework/spring-web ~spring-version :scope "provided"]]
   :java-source-paths ["src-java"]
   :repl-options {:init-ns org.msync.spring-boot-bugger}
   :aot [org.msync.spring-boot-bugger]
