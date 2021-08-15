17be4238074c4e8b73827540161054dfac373e84
Ravindra R. Jaju <jaju@msync.org>
2021-08-15 09:41:15 +0530

    Updated spring versions, introduced javadocs, bumped minor version

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(def spring-version "5.3.7")
+(def version "0.2.0-SNAPSHOT")
+(def spring-version "5.3.9")
+(def spring-boot-version "2.5.3")
 
-(defproject org.msync/spring-boot-bugger "0.1.1-SNAPSHOT"
+(defproject org.msync/spring-boot-bugger version
 
   :description "Just add dependency, sprinkle some config, and run Clojure in your Springboot application."
 
@@ -16,24 +18,48 @@
   :profiles {:provided
              {:dependencies
               [[org.springframework/spring-context ~spring-version]
-               [org.springframework/spring-web ~spring-version]]}}
+               [org.springframework/spring-webflux ~spring-version]]}
 
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [nrepl/nrepl "0.8.3"]]
+             :dev
+             {:dependencies
+              [[org.springframework.boot/spring-boot-starter-webflux ~spring-boot-version]]}}
+
+  :plugins [[org.msync/lein-javadoc "0.4.0-SNAPSHOT"]]
 
   :source-paths ["src" "src-java"]
+
   :java-source-paths ["src-java"]
 
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [nrepl "0.8.3"]]
+
+  :javadoc-opts {:package-names ["org.msync.spring_boot_bugger"]
+                 :additional-args ["-windowtitle" "SpringBoot Bugger Javadoc"
+                                   "-quiet"
+                                   "-link" "https://docs.oracle.com/en/java/javase/11/docs/api/"
+                                   "-link" "https://www.javadoc.io/static/org.clojure/clojure/1.10.3"
+                                   "-link" ~(str "https://javadoc.io/doc/org.springframework/spring-beans/" spring-version)
+                                   "-link" ~(str "https://javadoc.io/doc/org.springframework/spring-web/" spring-version)
+                                   "-link" "https://projectreactor.io/docs/core/release/api/"]}
+
+  :classifiers {:sources {:prep-tasks ^:replace []}
+                :javadoc {:prep-tasks ^:replace ["javadoc"]
+                          :omit-source true
+                          :filespecs ^:replace [{:type :path, :path "javadoc"}]}}
+
   :repl-options {:init-ns org.msync.spring-boot-bugger}
+
   :aot [org.msync.spring-boot-bugger]
 
   :jar-inclusions [#"spring-boot-bugger-*-sources.jar"]
 
-  :deploy-repositories [#_["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                                     :creds :gpg}]
-                        #_["snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
-                                      :creds :gpg}]
-                        ["releases" :clojars]
-                        ["snapshots" :clojars]]
+  :repositories {"snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots"}}
+
+  :deploy-repositories {
+                        "releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2" :creds :gpg}
+                        "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots" :creds :gpg}
+                        ;;"releases" :clojars
+                        ;;"snapshots" :clojars
+                        }
 
   )
