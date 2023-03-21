09993a002011f369b44a6c729cd61983feee2060
Ravindra Jaju <jaju@msync.org>
2023-03-21 22:35:28 +0530

    Releasing v0.2.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(def version "0.2.0-SNAPSHOT")
+(def version "0.2.0")
 (def core-version "1.11.1")
 (def spring-version "5.3.25")
-(def spring-boot-version "2.7.7")
+(def spring-boot-version "2.7.8")
 
 (defproject org.msync/spring-boost version
 
@@ -10,11 +10,14 @@
   :url "https://github.com/jaju/spring-boost"
 
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url "https://www.eclipse.org/legal/epl-2.0/"
+            :distribution :repo}
 
   :scm {:name "git" :url "https://github.com/jaju/spring-boost"}
 
-  :pom-addition [:developers [:developer [:name "Ravindra R. Jaju"]]]
+  :pom-addition [:developers [:developer
+                              [:id "jaju"]
+                              [:name "Ravindra R. Jaju"]]]
 
   :profiles {:provided
              {:dependencies
@@ -41,16 +44,22 @@
   :javadoc-opts {:package-names ["org.msync.spring_boost"]
                  :additional-args ["-windowtitle" "Spring Boost Javadoc"
                                    "-quiet"
-                                   "-link" "https://docs.oracle.com/en/java/javase/11/docs/api/"
+                                   "-link" "https://docs.oracle.com/en/java/javase/17/docs/api/"
                                    "-link" ~(str "https://www.javadoc.io/static/org.clojure/clojure/" core-version)
                                    "-link" ~(str "https://javadoc.io/doc/org.springframework/spring-beans/" spring-version)
                                    "-link" ~(str "https://javadoc.io/doc/org.springframework/spring-web/" spring-version)
                                    "-link" "https://projectreactor.io/docs/core/release/api/"]}
 
-  :classifiers {:sources {:prep-tasks ^:replace []}
+  :classifiers {:sources {:prep-tasks ^:replace []
+                          :source-paths ^:replace ["src"]
+                          :java-source-paths ^:replace ["src-java"]
+                          :resource-paths ^:replace []}
                 :javadoc {:prep-tasks ^:replace ["javadoc"]
                           :omit-source true
-                          :filespecs ^:replace [{:type :path, :path "javadoc"}]}}
+                          :filespecs ^:replace [{:type :path, :path "javadoc"}]
+                          :source-paths ^:replace ["src"]
+                          :java-source-paths ^:replace ["src-java"]
+                          :resource-paths ^:replace ["javadoc"]}}
 
   :repl-options {:init-ns org.msync.spring-boost}
 
@@ -61,8 +70,8 @@
   :repositories {"snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots"}}
 
   :deploy-repositories {
-                        "releases" :clojars #_{:url "https://oss.sonatype.org/service/local/staging/deploy/maven2" :creds :gpg}
-                        "snapshots" :clojars #_{:url "https://oss.sonatype.org/content/repositories/snapshots" :creds :gpg}
+                        "releases" #_:clojars {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/" :creds :gpg}
+                        "snapshots" #_:clojars {:url "https://oss.sonatype.org/content/repositories/snapshots/" :creds :gpg}
                         }
 
   )
